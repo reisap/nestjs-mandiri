3d868e021932c9f11ed159268974fcae387a7069
Reisa Prasaptaraya <r.prasaptaraya@gmail.com>
2024-04-04 01:17:55 +0700

    learning until admin auth

---
+++ noted.md
@@ -0,0 +1,9 @@
+1. harus ada Dockerfile terlebih dahulu
+
+Running docker
+
+```
+docker-compose up
+```
+
+2. if using database make sure not using "useSLL == false"
---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post()
+  @HttpCode(204)
+  createGreeting(): string {
+    return this.appService.createGreeting();
+  }
+}
